0a9887061d5dcd5d6625fc004a728df7a3fa1699
Gopal Tayade <tayadeg47@gmail.com>
2024-08-20 12:02:35 +0530

    update the time function to change dynamicaly

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ const monthNames = ["January", "February", "March" ,"April" , "May" , "June" ,"J
   const date = new Date();
   
   document.getElementById("date").innerHTML = `${date.toDateString()}`;
-  document.getElementById("time").innerHTML=`${date.toLocaleTimeString('en-US')}`;
+
+  //Time
+  function updateTime(){
+    const date = new Date();
+    document.getElementById("time").innerHTML=`${date.toLocaleTimeString('en-US')}`;
+  }
+  setInterval(updateTime, 1000);
+ //  updateTime();
+
+  
+
+
+
